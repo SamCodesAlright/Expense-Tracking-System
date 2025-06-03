d05591728964baa74a17a833c99780287c1db152
Sameer Shaikh <sameeritis10@gmail.com>
2025-06-03 14:27:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: process.env.CLIENT_URL,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
