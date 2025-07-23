6713dee3376cc36e76b0a362b3180814be6e85a1
Sameer Shaikh <sameeritis10@gmail.com>
2025-07-23 17:15:55 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-<<<<<<< HEAD
   try {
     await mongoose.connect(process.env.MONGO_URI, {});
     console.log("MongoDB Connected");
@@ -10,18 +9,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-=======
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
-        console.log("MongoDB Connected");
-    } catch(err) {
-        console.error("Error connecting to MongoDB", err);
-        process.exit(1);
-    }
-}
->>>>>>> 8273f5103fd85c6470a5520e5ae494151cf3da5d
 
 module.exports = connectDB;
